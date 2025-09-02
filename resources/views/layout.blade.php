8f884a393f465fbcf322c01872d15d11dd5b5dfe
nikita emelyanov <nik.emelyanov.1988@gmail.com>
2025-09-02 19:38:07 +0500

    add description

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">  
     <link rel="icon" href="favicon.ico">
-    <link rel="stylesheet" href="https://unpkg.com/swiper@10/swiper-bundle.min.css"/>
-    <link rel="icon" href="favicon.ico">
+    <meta name="description" content="Эффективная профессиональная психологическая помощь с использованием современных научно обоснованных методов: когнитивно-поведенческой терапии (КПТ) и других эффективных техник и методик.">  
 </head>
 <body>
     
