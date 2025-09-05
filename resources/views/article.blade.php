4985adb053a627e5a8931d958ee46fc3151c3cd3
nikita emelyanov <nik.emelyanov.1988@gmail.com>
2025-09-05 21:05:51 +0500

    update

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,12 +7,14 @@
         <div class="articles-inner wrapper">
             <h2 class="black-28px">{{$article->name}}</h2>
             <p class="black-18px-lh155">{!! nl2br(e($article->text)) !!}</p>
-            <a href="{{route('articleCreate.edit', $article->id)}}" class="white-19px btn-blue" style="position:absolute; margin-top: 15px">Изменить</a>
-            <form action="{{ route('articleCreate.destroy', $article->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="white-19px btn-delete" style="position:absolute; margin: 15px 0 0 150px; cursor: pointer;">Удалить</button>
-            </form>
+            @auth
+                <a href="{{route('articleCreate.edit', $article->id)}}" class="white-19px btn-blue" style="position:absolute; margin-top: 15px">Изменить</a>
+                <form action="{{ route('articleCreate.destroy', $article->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="white-19px btn-delete" style="position:absolute; margin: 15px 0 0 150px; cursor: pointer;">Удалить</button>
+                </form>
+            @endauth
         </div>
     </div>
 @endsection
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Sitemap\SitemapGenerator;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('sitemap:generate', function () {
+    SitemapGenerator::create(config('app.url'))
+        ->writeToFile(public_path('sitemap.xml'));
+
+    $this->info('Sitemap generated!');
+});
+
